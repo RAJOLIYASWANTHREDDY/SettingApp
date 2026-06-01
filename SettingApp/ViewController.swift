9f953523adb83fa58868961f8d8c6e3df338f6e4
YASWANTH <74554224+RAJOLIYASWANTHREDDY@users.noreply.github.com>
2026-06-01 09:45:44 -0700

    Initial Commit

---
+++ SettingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SettingApp
+//
+//  Created by yaswanth reddy on 6/1/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
